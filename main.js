5f103bd643333bd0a9a78ecc90e9e13946b2b49d
Antonio <antoniocutri841@gmail.com>
2025-11-19 00:01:35 +0100

    add counter in paraghraph

--- main.js
+++ main.js
@@ -20,3 +20,41 @@ window.addEventListener('scroll', ()=>{
         
     }
 })
+
+
+let counter = 0
+
+let span_sell = document.querySelector('#span_sell')
+let span_client = document.querySelector('#span_client')
+
+let confirm = true
+
+
+function custom_interval(max,span , time){
+
+    let interval = setInterval(()=>{
+        if (counter < max) {
+            counter++
+            span.innerHTML=counter
+        }else{
+            clearInterval(interval)
+        }
+    },time)
+
+    setTimeout(()=>{
+        confirm = true
+    }, 8000)
+}
+
+let observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        if (entry.isIntersecting && confirm) {
+            custom_interval(200, span_sell, 50)
+            custom_interval(180, span_client, 10) 
+            confirm = false
+        }
+        
+    });
+})
+
+observer.observe(span_sell)
