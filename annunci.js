3c83432ad1d76d28d8e0285a21c823007d7b7fd5
Antonio <antoniocutri841@gmail.com>
2025-11-19 17:50:14 +0100

    add filter by word

--- annunci.js
+++ annunci.js
@@ -1,40 +1,7 @@
-let navbar = document.querySelector('#navbar')
-let links = document.querySelectorAll('.nav-link')
-
-window.addEventListener('scroll', ()=>{
-    if (window.scrollY > 10) {
-        console.log(window.scrollY)
-        navbar.classList.remove('bg-blu')
-        navbar.classList.add('bg-green')
-        navbar.style.height = '130px' 
-        links.forEach(link => {
-            link.style.color = 'var(--dark_grey)'
-        });
-        
-    }else{
-        navbar.classList.remove('bg-green')
-        navbar.classList.add('bg-blu') 
-        links.forEach(link => {
-            link.style.color = 'var(--orange)'
-        });
-        navbar.style.height = '70px' 
-        
-    }
-})
-const toggler = document.querySelector('.navbar-toggler');
-toggler.addEventListener('click', () => {
-    const isOpen = toggler.getAttribute('aria-expanded') === "true";
-
-    if (isOpen) {
-        navbar.style.height = "200px";   // altezza quando aperto
-    } else {
-        navbar.style.height = "70px";    // altezza normale
-    }
-});
-
-
 
 fetch('./annunci.json').then((response)=> response.json() ).then((data)=>{
+    data.sort((a,b) => b.price - a.price)
+    
     let radio = document.querySelector('#radio')
     let card= document.querySelector('#card-announce')
     
@@ -59,21 +26,89 @@ fetch('./annunci.json').then((response)=> response.json() ).then((data)=>{
         });
     }
 
+    function truncate_words(string) {
+        if (string.length > 15) {
+           return string.split(' ')[0] + ' ...'
+        }
+        return string
+    }
+
     function createCard(data) {
+        card.innerHTML=''
         data.forEach(element => {
             let div = document.createElement('div')
             div.classList.add('card-body')
             div.innerHTML= `<div class="card border card-announce border-info bg-orange rounded-4 text-white d-flex align-items-center justify-content-center text-center m-2">
                     <div class="card-body  ">
-                        <h5 class="card-title mt-4">${element.name}</h5>
+                        <img src="https://picsum.photos/${element.id}" class="img-fluid img-card" alt="immagin casuale"> 
+                        <h5 class="card-title mt-2">${truncate_words(element.name)}</h5>
                         <p class="card-text">${element.price}</p>
                         <p class="card-text"> ${element.category}</p>
                     </div>
                 </div>`
             card.appendChild(div)
         });
-        
+    }
+
+    function filter_Category(category) {
+
+        if (category != 'all') {
+            let filterd = data.filter((announce)=> announce.category == category)
+            console.log(filterd)
+            createCard(filterd)
+        } else{
+            createCard(data)
+        }
         
     }
+
+    let radioButtons = document.querySelectorAll('.form-check-input')
+
+    radioButtons.forEach((button)=>{
+        button.addEventListener('click',()=>{
+            filter_Category(button.id)
+        })
+    })
+
+    let range= document.querySelector('#range')
+    let price_value = document.querySelector('#price_value')
+
+    range.addEventListener('input', ()=>{
+        filter_price()
+        price_value.textContent= range.value
+    })
+
+    price_input()
+
+    function price_input() {
+        let prices = data.map((announce)=> +announce.price)
+
+        prices.sort((a,b) => a - b)
+        let maxPrice = Math.ceil(prices.pop())
+
+        range.max = maxPrice
+        range.value = maxPrice
+        price_value.textContent= maxPrice
+        
+    }
+
+    function filter_price() {
+        let filterd = data.filter((announce)=> +announce.price <= range.value)
+        createCard(filterd)
+    }
+
+
+    let input_word = document.querySelector('#input_word')
+
+    function filter_word(string) {
+        let filtered = data.filter((announce)=> announce.name.toLowerCase().includes(string.toLowerCase()) )
+        createCard(filtered)
+
+    }
+
+    input_word.addEventListener('input', ()=>{
+        filter_word(input_word.value)
+    })
+
 
 })
