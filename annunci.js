c1ca944373feaabe6073bb5774761e157ed91916
Antonio <antoniocutri841@gmail.com>
2025-11-19 16:48:19 +0100

    add annauncment card

--- annunci.js
+++ annunci.js
@@ -31,3 +31,49 @@ toggler.addEventListener('click', () => {
         navbar.style.height = "70px";    // altezza normale
     }
 });
+
+
+
+fetch('./annunci.json').then((response)=> response.json() ).then((data)=>{
+    let radio = document.querySelector('#radio')
+    let card= document.querySelector('#card-announce')
+    
+    radioCreate(data)
+    createCard(data)
+    
+
+    function radioCreate(data){
+        
+        let categories = data.map( (announce)=> announce.category)
+
+        let unique = Array.from(new Set(categories))
+
+        unique.forEach(element => {
+            let div = document.createElement('div')
+            div.classList.add('form-check')
+            div.innerHTML=`<input class="form-check-input" type="radio" name="radioDefault" id="${element}">
+                                    <label class="form-check-label" for="${element}">
+                                    ${element}
+                                    </label>`
+            radio.appendChild(div)
+        });
+    }
+
+    function createCard(data) {
+        data.forEach(element => {
+            let div = document.createElement('div')
+            div.classList.add('card-body')
+            div.innerHTML= `<div class="card border card-announce border-info bg-orange rounded-4 text-white d-flex align-items-center justify-content-center text-center m-2">
+                    <div class="card-body  ">
+                        <h5 class="card-title mt-4">${element.name}</h5>
+                        <p class="card-text">${element.price}</p>
+                        <p class="card-text"> ${element.category}</p>
+                    </div>
+                </div>`
+            card.appendChild(div)
+        });
+        
+        
+    }
+
+})
